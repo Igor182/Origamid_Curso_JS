0900d08a80e2aade4dea0d9b423e7cba1680b423
Anderson Silva <igormello_2014@hotmail.com>
2022-12-02 12:20:20 -0300

    mais uma atualização do projeto de JS

--- js/modules/tabnav.js
+++ js/modules/tabnav.js
@@ -1,10 +1,10 @@
-export default function initTab() {
+export default function initTabNav() {
   const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
   const tabContent = document.querySelectorAll('[data-tab="content"] section');
-  
+
   if(tabMenu.length && tabContent.length) {
     tabContent[0].classList.add('ativo');
-  
+
     function activeTab(index) {
       tabContent.forEach((section) => {
         section.classList.remove('ativo');
@@ -12,7 +12,7 @@ export default function initTab() {
       const direcao = tabContent[index].dataset.anime;
       tabContent[index].classList.add('ativo', direcao);
     }
-  
+
     tabMenu.forEach((itemMenu, index) => {
       itemMenu.addEventListener('click', () => {
         activeTab(index);
@@ -20,5 +20,3 @@ export default function initTab() {
     });
   }
 }
-
-initTab()
